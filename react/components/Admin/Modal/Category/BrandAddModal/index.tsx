cb1a88c305db39ec7bd26112e398441ac4df4b8d
Linh <quanglinh1286@gmail.com>
2025-12-11 04:32:51 +0700

    update 1.0

--- react/components/Admin/Modal/Category/BrandAddModal/index.tsx
+++
@@ -1,7 +0,0 @@
-import { memo } from "react";
-
-const BrandAddModal = () => {
-  return <div>BrandAddModal</div>;
-};
-
-export default memo(BrandAddModal);
--- react/utils/formatVND.ts
+++
@@ -1,5 +0,0 @@
-const formatVND = (amount: number) => {
-    return new Intl.NumberFormat('vi-VN').format(amount) + ' đ';
-} 
-
-export default formatVND;
